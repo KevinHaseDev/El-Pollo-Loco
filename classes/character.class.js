af1907a316ffc2cc6b166ae5b88539c6d3c1a188
Kevin Hase <hasekevin21@googlemail.com>
2025-09-22 21:09:58 +0200

    start animate charakter and clouds

--- classes/character.class.js
+++ classes/character.class.js
@@ -3,15 +3,26 @@ class Character extends MovableObject {
 
     images_walking = [
         '../assets/img/2_character_pepe/2_walk/W-21.png',
-            '../assets/img/2_character_pepe/2_walk/W-22.png',
-            '../assets/img/2_character_pepe/2_walk/W-23.png',
-            '../assets/img/2_character_pepe/2_walk/W-24.png',
-            '../assets/img/2_character_pepe/2_walk/W-25.png',
-            '../assets/img/2_character_pepe/2_walk/W-26.png'
+        '../assets/img/2_character_pepe/2_walk/W-22.png',
+        '../assets/img/2_character_pepe/2_walk/W-23.png',
+        '../assets/img/2_character_pepe/2_walk/W-24.png',
+        '../assets/img/2_character_pepe/2_walk/W-25.png',
+        '../assets/img/2_character_pepe/2_walk/W-26.png'
     ];
+    currentImage = 0;
     constructor() {
         super(120, 400).loadImage('../assets/img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.images_walking);
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let path = this.images_walking[this.currentImage];
+            this.img = this.imageChache[path];
+            this.currentImage++;
+        }, 1000);
     }
 
     jump() {
