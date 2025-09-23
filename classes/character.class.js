1236a075e5f8a60f8a2aa2c22fddd5510a4dcd05
Kevin Hase <hasekevin21@googlemail.com>
2025-09-23 22:37:01 +0200

    animate walking charakter

--- classes/character.class.js
+++ classes/character.class.js
@@ -10,12 +10,44 @@ class Character extends MovableObject {
         '../assets/img/2_character_pepe/2_walk/W-26.png'
     ];
     currentImage = 0;
+    world;
+
     constructor() {
         super(120, 400).loadImage();
-         this.loadImage(this.images_walking[0]);
+        this.loadImage(this.images_walking[0]);
         this.loadImages(this.images_walking);
 
-        this.animate(this.images_walking, 7000 / 60);
+        this.animate();
+    }
+
+    /**
+     * Führt die Geh-Animation aus und bewegt die Figur nach links oder rechts.
+     * (Deutsch: Prüft keyboard.right und keyboard.left in world und passt x sowie das angezeigte Bild an.)
+     */
+    animate() {
+        setInterval(() => {
+            if (!this.world || !this.world.keyboard) return;
+
+            // Wenn nach rechts gedrückt wird
+            if (this.world.keyboard.right) {
+                this.x += this.speed * 60;
+                this.otherDirection = false;
+                let i = this.currentImage % this.images_walking.length;
+                let path = this.images_walking[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+            // Wenn nach links gedrückt wird
+            else if (this.world.keyboard.left) {
+                this.x -= this.speed * 60;
+                this.otherDirection = true;
+                let i = this.currentImage % this.images_walking.length;
+                let path = this.images_walking[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+            this.world.camera_x = -this.x + 120;
+        }, 5000 / 60);
     }
 
     jump() {
--- classes/world.class.js
+++ classes/world.class.js
@@ -16,23 +16,32 @@ class World {
     ];
     canvas;
     ctx;
-    keyboard
+    keyboard;
+    camera_x = 0;
+
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext("2d");
         this.canvas = canvas;
         this.keyboard = keyboard;
         this.draw();
+        this.setWorld();
     }
 
-
+    setWorld() {
+        this.character.world = this;
+    }
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // clear canvas
+
+        this.ctx.translate(this.camera_x, 0); // Kamera verschieben
 
         this.addObjectToMap(this.backgroundObjects);
         this.addObjectToMap(this.clouds);
         this.addObjectToMap(this.enemies);
         this.addToMap(this.character);
+
+        this.ctx.translate(-this.camera_x, 0); // Kamera zurücksetzen
 
 
 
@@ -48,7 +57,19 @@ class World {
         });
     }
 
-    addToMap(movableObject) {
-        this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
+    addToMap(mo) {
+        this.ctx.save();
+
+        if (mo.otherDirection) {
+            // nach links spiegeln
+            this.ctx.translate(mo.x + mo.width, mo.y);
+            this.ctx.scale(-1, 1);
+            this.ctx.drawImage(mo.img, 0, 0, mo.width, mo.height);
+        } else {
+            this.ctx.translate(mo.x, mo.y);
+            this.ctx.drawImage(mo.img, 0, 0, mo.width, mo.height);
+        }
+
+        this.ctx.restore();
     }
 }
